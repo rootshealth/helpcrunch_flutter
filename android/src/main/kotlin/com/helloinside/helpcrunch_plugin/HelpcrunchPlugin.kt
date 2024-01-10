80b2d3442d5aac05f667adb4be35b5881646a291
Martin Kamleithner <martin.kamleithner@gmail.com>
2024-01-10 02:24:03 +0000

    update

--- android/src/main/kotlin/com/helloinside/helpcrunch_plugin/HelpcrunchPlugin.kt
+++ android/src/main/kotlin/com/helloinside/helpcrunch_plugin/HelpcrunchPlugin.kt
@@ -1,13 +1,10 @@
 package com.helloinside.helpcrunch_plugin
 
 import androidx.annotation.NonNull
-import com.google.firebase.messaging.RemoteMessage
 import com.helpcrunch.library.core.Callback
 import com.helpcrunch.library.core.HelpCrunch
 import com.helpcrunch.library.core.models.user.HCUser
 import com.helpcrunch.library.core.options.HCOptions
-import com.helpcrunch.library.core.options.HCPreChatForm
-import com.helpcrunch.library.core.options.design.HCPreChatTheme
 import com.helpcrunch.library.core.options.theme.HCNotificationsTheme
 import com.helpcrunch.library.core.options.theme.HCTheme
 
@@ -38,21 +35,21 @@ class HelpcrunchPlugin : FlutterPlugin {
  */
 private class HelpCrunchPluginImpl : Pigeon.HelpCrunchPlugin {
 
-    override fun showChatScreen(resultCallback: Pigeon.Result<Void>?) {
+    override fun showChatScreen(resultCallback: Pigeon.Result<Void>) {
         HelpCrunch.showChatScreen(
             options = null,
             callback = object : Callback<Any?>() {
                 override fun onSuccess(result: Any?) {
-                    resultCallback?.success(null)
+                    resultCallback.success(null)
                 }
 
                 override fun onError(message: String) {
-                    resultCallback?.error(Exception(message))
+                    resultCallback.error(Exception(message))
                 }
             })
     }
 
-    override fun updateUser(user: Pigeon.HelpCrunchUser, resultCallback: Pigeon.Result<Void>?) {
+    override fun updateUser(user: Pigeon.HelpCrunchUser, resultCallback: Pigeon.Result<Void>) {
         HelpCrunch.updateUser(
             user = HCUser.Builder().apply {
                 userId = user.id
@@ -64,47 +61,47 @@ private class HelpCrunchPluginImpl : Pigeon.HelpCrunchPlugin {
             }.build(),
             callback = object : Callback<HCUser>() {
                 override fun onSuccess(result: HCUser) {
-                    resultCallback?.success(null)
+                    resultCallback.success(null)
                 }
 
                 override fun onError(message: String) {
-                    resultCallback?.error(Exception(message))
+                    resultCallback.error(Exception(message))
                 }
             })
     }
 
-    override fun logoutUser(resultCallback: Pigeon.Result<Void>?) {
+    override fun logoutUser(resultCallback: Pigeon.Result<Void>) {
         HelpCrunch.logout(callback = object : Callback<Any?>() {
             override fun onSuccess(result: Any?) {
-                resultCallback?.success(null)
+                resultCallback.success(null)
             }
 
             override fun onError(message: String) {
-                resultCallback?.error(Exception(message))
+                resultCallback.error(Exception(message))
             }
         })
     }
 
-    override fun registerForRemoteMessages(result: Pigeon.Result<Void>?) {
-        result?.error(Exception("not yet implemented"))
+    override fun registerForRemoteMessages(result: Pigeon.Result<Void>) {
+        result.error(Exception("not yet implemented"))
     }
 
-    override fun getNumberOfUnreadChats(resultCallback: Pigeon.Result<Long>?) {
+    override fun getNumberOfUnreadChats(resultCallback: Pigeon.Result<Long>) {
 
         HelpCrunch.getUnreadChatsCount(object : Callback<Int>() {
             override fun onSuccess(result: Int) {
-                resultCallback?.success(result.toLong())
+                resultCallback.success(result.toLong())
             }
 
             override fun onError(message: String) {
-                resultCallback?.error(Exception(message))
+                resultCallback.error(Exception(message))
             }
         })
     }
 
     override fun initialize(
         params: Pigeon.HelpCrunchInitializationParams,
-        resultCallback: Pigeon.Result<Void>?
+        resultCallback: Pigeon.Result<Void>
     ) {
         val color = params.notificationColor
 
@@ -140,11 +137,11 @@ private class HelpCrunchPluginImpl : Pigeon.HelpCrunchPlugin {
             options = options,
             callback = object : Callback<Any>() {
                 override fun onSuccess(result: Any) {
-                    resultCallback?.success(null)
+                    resultCallback.success(null)
                 }
 
                 override fun onError(message: String) {
-                    resultCallback?.error(Exception(message))
+                    resultCallback.error(Exception(message))
                 }
             })
     }
